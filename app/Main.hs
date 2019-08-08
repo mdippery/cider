4cfd4417ee45bab37157555bff474e8c5d0bc950
Michael Dippery <michael@monkey-robot.com>
2019-08-08 10:21:42 -0700

    Create basic skeleton of executable

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,41 @@
 module Main where
 
+import Data.Version          (showVersion)
+import System.Environment    (getArgs, getProgName)
+import System.Exit           (ExitCode(..), exitWith)
+import Text.Printf           (printf)
+
+import qualified Paths_pyxis as P
+
+help :: IO ()
+help = do
+  exe <- getProgName
+  putStrLn $ printf "Usage: %s <cidr block>" exe
+
+version :: IO ()
+version = do
+  exe <- getProgName
+  let v = showVersion P.version
+  putStrLn $ printf "%s v%s" exe v
+
+die :: Int -> IO ()
+die 0    = exitWith ExitSuccess
+die code = exitWith (ExitFailure code)
+
 main :: IO ()
-main = putStrLn "Hello, world!"
+main = do
+  argv <- getArgs
+  case argv of
+    ("-h":_) ->
+      help
+    ("--help":_) ->
+      help
+    ("-V":_) ->
+      version
+    ("--version":_) ->
+      version
+    (ip:_) ->
+      putStrLn ip
+    _ -> do
+      help
+      die 1
