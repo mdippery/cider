a831c979aee75e5e1ce7e315316ad081fa24bb13
Michael Dippery <michael@monkey-robot.com>
2019-08-08 10:38:46 -0700

    Print all addresses for a given CIDR block

--- app/Main.hs
+++ app/Main.hs
@@ -1,11 +1,19 @@
 module Main where
 
+import Data.List             (intercalate)
 import Data.Version          (showVersion)
 import System.Environment    (getArgs, getProgName)
 import System.Exit           (ExitCode(..), exitWith)
+import System.IO             (hPutStrLn, stderr)
 import Text.Printf           (printf)
+import Text.Read             (readMaybe)
+
+import Saturn.Network (IPAddressRange, addresses)
 
 import qualified Paths_pyxis as P
+
+readBlock :: String -> Maybe IPAddressRange
+readBlock = readMaybe
 
 help :: IO ()
 help = do
@@ -34,8 +42,13 @@ main = do
       version
     ("--version":_) ->
       version
-    (ip:_) ->
-      putStrLn ip
+    (ip:_) -> do
+      case readBlock ip of
+        Just r ->
+          putStrLn $ intercalate "\n" $ map show $ addresses r
+        Nothing -> do
+          hPutStrLn stderr $ "Invalid CIDR block: " ++ show ip
+          die 2
     _ -> do
       help
       die 1
